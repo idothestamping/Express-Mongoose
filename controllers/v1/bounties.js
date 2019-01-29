72e5d727156aca01f66ee3924aea97af5062d59e
idothestamping <douglas.klemp@gmail.com>
2019-01-29 12:08:16 -0800

    Prepping code for deployment

--- controllers/v1/bounties.js
+++ controllers/v1/bounties.js
@@ -20,5 +20,42 @@ router.get('/', (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+    // res.send('Post Reached!');
+    req.body.hunters = JSON.parse(req.body.hunters);
+    db.Bounty.create(req.body)
+    .then(bounty => {
+        res.status(201).send(bounty);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).send({ message: 'Server Error' });
+    });
+});
+
+router.get('/:id', (req, res) => {
+    // res.send('GET /:id Reached!');
+    db.Bounty.findById(req.param.id)
+    .then(bounty => {
+        res.send(bounty)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).send({ message: 'Server Error' });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    // res.send('DELETE /:id Reached!');
+    db.Bounty.findByIdAndDelete({ _id: req.params.id })
+    .then(() => {
+        res.status(204).send({ message: 'Successful Deletion' });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).send({ message: 'Server Error' });
+    });
+});
+
 // Export this router to be included by index.js - or yourfilename.js
 module.exports = router;
